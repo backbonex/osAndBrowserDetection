cba0d0b694a609de7fad916abdaf755e35c51916
Andrew Balakirev <andrew.balakirev@datravel.com>
2015-01-12 12:22:21 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # osAndBrowserDetection
 Detects OS and brower and writes CSS class to html element
+
+Browser classes:
+    mozilla
+    msie
+    opera
+    webkit
+
+OS classes:
+    win
+    mac
+    x11 for UNIX
+    linux
---
+++ osAndBrowserDetection.js
@@ -0,0 +1,16 @@
+define(['underscore', 'jquery'], function (_, $) {
+    "use strict";
+
+    var $html = $('html');
+    // adds browser (mozilla, msie, opera, webkit) to html class
+    var browser = _.chain($.browser).keys().without('version').value()[0];
+    if (browser) {
+        $html.addClass(browser);
+    }
+
+    // adds OS to html class, x11 for UNIX
+    var os = navigator.appVersion.toLowerCase().match(/win|mac|x11|linux/);
+    if (os) {
+        $html.addClass(os[0]);
+    }
+});
